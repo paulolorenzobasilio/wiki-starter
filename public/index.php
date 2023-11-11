3a9872c60569e039bd4cc1a6c91ed228928e1e21
Paulo Basilio <paulo.basilio@lamudi.com>
2023-11-12 02:56:47 +0800

    chore: server config

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use App\Controller\WikiController;
 use App\Middleware\WikiValidatorMiddleware;
 use DI\Bridge\Slim\Bridge;
 use Doctrine\ORM\EntityManager;
+use Slim\Exception\HttpNotFoundException;
 use Symfony\Component\Serializer\Serializer;
 use Whoops\Handler\JsonResponseHandler;
 use Zeuxisoo\Whoops\Slim\WhoopsMiddleware;
@@ -22,10 +23,24 @@ $container = $builder->build();
 
 $app = Bridge::create($container);
 
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->add(new WhoopsMiddleware([], [new JsonResponseHandler()]));
+
 
 $app->get('/', WikiController::class . ':get');
 $app->post('/', WikiController::class . ':post')->add(new WikiValidatorMiddleware());
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+    throw new HttpNotFoundException($request);
+});
 
 $app->run();
 
