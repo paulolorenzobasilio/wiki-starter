3ab7e3adb3beea2e516695d4264cb94b15c869ec
Paulo Basilio <paulo.basilio@lamudi.com>
2023-11-12 04:37:08 +0800

    chore: post wiki api frontend

--- www/app.js
+++ www/app.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom'
+import axios from "axios";
 
 function App(){
+    const [formData, setFormData] = useState({
+        title: "",
+        description: ""
+    })
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        formData.description = editor.getText()
+        try {
+            const bodyFormData = new FormData();
+            bodyFormData.append('title', formData.title)
+            bodyFormData.append('description', formData.description)
+            const response = await axios({
+                method: 'post',
+                url: 'https://127.0.0.1:8000',
+                data: bodyFormData,
+                headers: {
+                    'Content-Type': `multipart/form-data`
+                }
+            })
+            console.log('Success', response.data)
+            alert('Success')
+        } catch (error){
+            alert('Error')
+            console.error("Error:", error)
+        }
+
+    }
     return (
         <div className={"container"}>
-            <div className="mb-3">
-                <label className="form-label">Title</label>
-                <input type="text" className="form-control" id="tile" placeholder="Enter wiki title..."/>
-            </div>
-            <div className="mb-3">
-                <label className="form-label">Description</label>
-                <div id="description" className={"quill-editor"}></div>
-            </div>
-            <button className={"btn btn-primary"}>Submit</button>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label className="form-label">Title</label>
+                    <input id="title" type="text" className="form-control"
+                           placeholder="Enter wiki title..."
+                           value={formData.title}
+                           onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Description</label>
+                    <div id="description" className={"quill-editor"}/>
+                </div>
+                <button className={"btn btn-primary"} type={"submit"}>Submit</button>
+            </form>
         </div>
     );
 }
